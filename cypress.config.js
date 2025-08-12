19669812a7332de0ff6831ff3f5359681b693180
Kakhaber Makharashvili <k.makharashvili85@gmail.com>
2025-08-12 19:46:47 +0400

    10

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: "https://testzootopia.loremipsum.ge/ka",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
