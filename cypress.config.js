c3170c870daef54e541913208a7dd2020a3d8c78
Kakhaber Makharashvili <k.makharashvili85@gmail.com>
2025-08-12 19:20:10 +0400

    km

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    specPattern: "cypress/e2e/**/*.spec.js",
+    supportFile: "cypress/support/e2e.js",
+    baseUrl: "https://testzootopia.loremipsum.ge/ka",
   },
 });
